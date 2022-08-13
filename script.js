027679125de172c921d57f95a8fe26f1bb03fbc8
Necryl <necrylthewayfarer@gmail.com>
2022-08-13 18:30:10 +0530

    Added js and css

--- script.js
+++ script.js
@@ -1,28 +1,101 @@
 // get elements
-const body = document.querySelector("body");
-const beforeGameInput = document.querySelector(".beforeGame input");
+const bodyElement = document.querySelector("body");
+const beforeGameInputElement = document.querySelector(".beforeGame input");
+const beforeGameElement = document.querySelector(".beforeGame");
+const currentRoundElement = document.querySelector(".currentRound");
+const suspenseElements = document.querySelectorAll(".suspense");
+const inGameElement = document.querySelector('.inGame');
+const endGameElement = document.querySelector('.endGame');
+const mainMenuBtnElement = document.querySelector('.mainMenu');
+const playerInputBtns = [
+    document.querySelector('.rock'),
+    document.querySelector('.paper'),
+    document.querySelector('.scissors')
+]
+const roundNumElement = document.querySelector('.roundNum');
+const verdictElement = document.querySelector('.verdict');
+const arenaElement = document.querySelector('.arena');
+const scoreElement = document.querySelector('.scoreElement');
+const scoreDataElements = document.querySelectorAll('.scoreData');
+const historyElement = document.querySelector('.history');
 
-let choices = ["Rock", "Paper", "Scissors"];
+// data variables
+const choices = ["Rock", "Paper", "Scissors"];
+
+// VARIABLE DEFAULTS
+// configuration variables
+let numOfRounds_default = 5;
+// status variables
+let makingChoice_default = false;
+let currentRound_default;
+let scores_default = [0, 0, 0];
+let currentRound_status_default;
+// other variables
+let arenaInterval_default;
+
+
+// configuration variables
+let numOfRounds = numOfRounds_default;
+// status variables
+let makingChoice = makingChoice_default;
+let currentRound = currentRound_default;
+let scores = scores_default.slice(0);
+let currentRound_status = currentRound_status_default;
+// other variables
+let arenaInterval = arenaInterval_default;
 
 
 // attaching events
-beforeGameInput.addEventListener("input", setInputWidth);
-beforeGameInput.addEventListener("change", event => {
-    if (beforeGameInput.value < 1) {
-        beforeGameInput.value = 1;
+beforeGameInputElement.addEventListener("input", setInputWidth);
+beforeGameInputElement.addEventListener("change", event => {
+    if (beforeGameInputElement.value < 1) {
+        beforeGameInputElement.value = 1;
         setInputWidth();
     }
 });
+playerInputBtns.forEach((btn, index) => {
+    btn.addEventListener("click", event => {
+        closeRound(index);
+    });
+});
+mainMenuBtnElement.addEventListener('click', event => {
+    resetEverything();
+});
 
-
-// defining functions
-function setInputWidth () {
-    beforeGameInput.style.width = `${beforeGameInput.value.length + 2}ch`;
+// defining some functions
+function delay(ms) {
+    // use within an async function with await
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function gapIt() {console.log("-------------------------------------");}
+function setInputWidth() {
+    beforeGameInputElement.style.width = `${beforeGameInputElement.value.length + 2}ch`;
+}
 
-function getComputerChoice() {return choices[Math.floor(Math.random() * 2.99)];}
+function updateArenaInterval() {
+
+    stuff = '...'
+    if (!arenaInterval) {
+        arenaInterval = undefined;
+        arenaInterval = setInterval(() => {
+            if (stuff.length < 3) {
+                stuff += '.';
+            } else {
+                stuff = '';
+            }
+            suspenseElements.forEach(elem => {
+                elem.textContent = stuff
+            });
+        }, 375);
+    } else {
+        clearInterval(arenaInterval);
+        arenaInterval = false;
+    }
+}
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * 2.99);
+}
 
 function getVerdict(playerSelection, computerSelection) {
     let result;
@@ -51,44 +124,87 @@ function getVerdict(playerSelection, computerSelection) {
     return result;
 }
 
-function getPlayerChoice() {
-    let choice_status = false;
-    let user_input
-    while (!choice_status) {
-        user_input = prompt("You choose:").toLowerCase();
-        user_input = user_input.charAt(0).toUpperCase() + user_input.slice(1);
-        if (choices.includes(user_input)) {
-            choice_status = true;
+function setCurrentRound() {
+    if (arguments.length > 0) {
+        if (arguments.length === 1) {
+            verdictElement.textContent = arguments[0];
+
+            verdictElement.style.display = 'flex';
+            roundNumElement.style.display = 'none';
+            arenaElement.style.display = 'none';
         } else {
-            alert(`Sorry, but <${user_input}> is not a choice! Try again!`);
+            suspenseElements[0].textContent = arguments[0];
+            suspenseElements[1].textContent = arguments[1];
+
+            verdictElement.style.display = 'none';
+            roundNumElement.style.display = 'flex';
+            arenaElement.style.display = 'flex';
         }
-    }
-    return user_input;
-}
-
-function playRound(round) {
-    console.log(`Round ${round}: ${choices[0]}, ${choices[1]}, ${choices[2]}!`);
-    let playerPlays = getPlayerChoice();
-    console.log(`You played ${playerPlays}`);
-    let computerPlays = getComputerChoice();
-    console.log(`Computer plays ${computerPlays}`);
-    let verdict = getVerdict(playerPlays, computerPlays);
-    console.log(verdict);
-    if (verdict[4] === 'W') {
-        return 'won';
-    } else if (verdict[4] === 'l') {
-        return 'lost';
     } else {
-        return 'draw';
+        throw "Error: No arguments given";
     }
 }
 
-function game() {
-    let scores = [0, 0, 0];
-    let roundsNum = 5;
-    for (let i = 0; i < roundsNum; i++) {
-        gapIt();
-        let score = playRound(i + 1);
+async function addToHistory() {
+    let entry = document.createElement('div');
+    entry.classList.add('historyItem');
+    entry.style.height = '0';
+    entry.style.fontSize = '0';
+    let roundElem = roundNumElement.cloneNode(true);
+    roundElem.style.justifyContent = 'left';
+    entry.appendChild(roundElem);
+    entry.appendChild(arenaElement.cloneNode(true));
+    historyElement.prepend(entry);
+    await delay(100);
+    entry.style.height = 'auto';
+    entry.style.fontSize = 'initial';
+}
+
+// game play functions
+function startGame() {
+    numOfRounds = Number(beforeGameInputElement.value);
+    currentRound = 0;
+    beforeGameElement.style.display = "none";
+    currentRoundElement.style.display = "flex";
+    startRound();
+}
+
+async function startRound() {
+    makingChoice = true;
+    currentRound++;
+    setCurrentRound(`Round ${currentRound}/${numOfRounds}`)
+    await delay(1500);
+    roundNumElement.textContent = `Round ${currentRound}`;
+    setCurrentRound('', '');
+    inGameElement.style.display = "flex";
+    updateArenaInterval();
+}
+
+async function closeRound(playerChoice) {
+    if (makingChoice) {
+        let lastRound = false;
+        makingChoice = false;
+        inGameElement.style.display = "none";
+        updateArenaInterval();
+        let computerChoice = getComputerChoice();
+        let verdict =  getVerdict(choices[playerChoice], choices[computerChoice]);
+        playerChoice = choices[playerChoice];
+        computerChoice = choices[computerChoice];
+        setCurrentRound(computerChoice, playerChoice);
+        if (historyElement.style.display = 'none') {
+            historyElement.style.display = 'flex';
+        }
+        addToHistory();
+        await delay(1250);
+        setCurrentRound(verdict);
+        let score;
+        if (verdict[4] === 'W') {
+            score = 'won';
+        } else if (verdict[4] === 'l') {
+            score = 'lost';
+        } else {
+            score = 'draw';
+        }
         switch (score) {
             case "won":
                 scores[0] = scores[0] + 1;
@@ -100,11 +216,21 @@ function game() {
                 scores[2] = scores[2] + 1;
                 break;
             default:
-                console.log(`ERROR: switch in <function game> defaulted at loop ${i}`);
+                throw `ERROR: switch defaulted at round ${currentRound}. Score was <${score}> when it was supposed to be either "won", "lost" or "draw"`
         }
+        await delay(2000);
+        if (currentRound === numOfRounds) {
+            endGame();
+        } else {
+            startRound();
+        }
+
     }
+}
+
+async function endGame() {
+    
     let verdict;
-    gapIt();
     if (scores[0] > scores[1]) {
         verdict = "You win the game!";
     } else if (scores[0] < scores[1]) {
@@ -112,31 +238,43 @@ function game() {
     } else {
         verdict = "The game is a draw!";
     }
-    console.log(verdict);
-    gapIt();
-    console.log(`Your scores:`);
-    console.log(`Rounds played -- ${roundsNum}`);
-    console.log(`Wins ----------- ${scores[0]}`);
-    console.log(`Losses --------- ${scores[1]}`);
-    console.log(`Draws ---------- ${scores[2]}`);
+    setCurrentRound(verdict);
+    roundNumElement.textContent = verdict;
+    await delay(2000);
+    endGameElement.style.display = 'flex';
+    verdictElement.style.display = 'none';
+    roundNumElement.style.display = 'flex';
+    arenaElement.style.display = 'none';
+    scoreElement.style.display = 'flex';
+    for (let i = 0; i < scoreDataElements.length; i++) {
+        if (i === 0) {
+            scoreDataElements[i].textContent = currentRound;
+        } else {
+            scoreDataElements[i].textContent = scores[i - 1];
+        }
+    }
 }
 
-function play() {
-    body.classList.remove("centered");
+function resetEverything() {
+    endGameElement.style.display = 'none';
+    beforeGameElement.style.display = "flex";
+    inGameElement.style.display = "none";
+    currentRoundElement.style.display = "none";
+    scoreElement.style.display = 'none';
+    historyElement.innerHTML = '';
+    historyElement.style.display = 'none';
+
+    // Reseting variables
+    // configuration variables
+    numOfRounds = numOfRounds_default;
+    // status variables
+    makingChoice = makingChoice_default;
+    currentRound = currentRound_default;
+    scores = scores_default.slice(0);
+    currentRound_status = currentRound_status_default;
+    // other variables
+    arenaInterval = arenaInterval_default;
 }
 
 //on start
-body.classList.add("centered");
-beforeGameInput.value = 5;
-
-
-// console.log(`Welcome to Rock Paper Scissors!`);
-// alert("Welcome to Rock Paper Scissors!");
-// let play = confirm("Wanna play?");
-// while (play) {
-//     game();
-//     play = confirm("Wanna play again?");
-// }
-// gapIt();
-// console.log("Exiting");
-// gapIt();
+beforeGameInputElement.value = numOfRounds_default;
